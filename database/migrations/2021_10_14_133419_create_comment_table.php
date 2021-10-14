b3fda0bddfd3579b444c3d290f0c6bfeb88d4808
nguyenvanduc208 <dotkich208@gmail.com>
2021-10-14 21:28:45 +0700

    first commit

---
+++ database/migrations/2021_10_14_133419_create_comment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->text('post');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
