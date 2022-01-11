4fca2a286e76250a221786da5329e8c2c0e3095d
nguyenvanduc208 <dotkich208@gmail.com>
2022-01-11 17:50:43 +0700

    ads

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,7 +10,7 @@ class Booking extends Model
     use HasFactory;
     protected $table='booking';
     protected $fillable = [
-        'customer_id','resort_id','price','address',
+        'user_id','resort_id','price','address',
         'number_phone','check_in_date','check_out_date',
         'count_people','status','payment_img'
     ];
